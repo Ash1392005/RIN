35166a34f06094ff07a18e6c30a3fa723625f423
Ash1392005 <95693441+Ash1392005@users.noreply.github.com>
2022-02-26 08:06:23 +0530

    Update Help1.ts

--- src/commands/General/Help1.ts
+++ src/commands/General/Help1.ts
@@ -24,7 +24,7 @@ export default class Command extends BaseCommand {
         let well = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: well }, MessageType.video, {quoted:M.WAMessage,
             mimetype: Mimetype.gif,
-            caption: `-🄶🄴🄽🄴🅁🄰🄻 🄻🄸🅂🅃 🧣
+            caption: `-🄶🄴🄽🄴🅁🄰🄻 🄻🄸🅂🅃 ❤️
 ──────────────
 🕸️ ${this.client.config.prefix}ᴀᴅᴍɪɴꜱ
 🕷️ ${this.client.config.prefix}ᴇᴠᴇʀʏᴏɴᴇ

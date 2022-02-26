388df9366f8c4fc0352dd8c75981970d2b36a546
Ash1392005 <95693441+Ash1392005@users.noreply.github.com>
2022-02-26 08:30:44 +0530

    Update Rin.ts

--- src/commands/Misc/Rin.ts
+++ src/commands/Misc/Rin.ts
@@ -8,10 +8,10 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'rin',
+            command: 'spidy',
             description: 'Displays the info.',
             category: 'misc',
-            usage: `${client.config.prefix}rin`
+            usage: `${client.config.prefix}spidy`
         })
     }
 
@@ -22,7 +22,7 @@ export default class Command extends BaseCommand {
         let rin = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: rin }, MessageType.video, {quoted:M.WAMessage,
             mimetype: Mimetype.gif,
-            caption: `🎗 Rin\n\n⚜ *Description: Maintained fork of void* \n\n ⭐*URL-https://github.com/Oreki-san/RIN* \n` }
+            caption: `🎗 Rin\n\n⚜ *Description: just follow me* \n\n ⭐*Follow me on insta- https://www.instagram.com/reel/CUx2XrcBnVA/?utm_medium=copy_link* \n` }
         )
     }
 }
